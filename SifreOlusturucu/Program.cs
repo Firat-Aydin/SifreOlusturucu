2acf4f9676245725689bf7209f5aba33adb942a3
Honor <Honor@LAPTOP-71C6DBHA>
2023-04-21 23:12:47 +0300

    Add project files.

---
+++ SifreOlusturucu/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SifreOlusturucu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sifre uzunluğunu girin :");
+            int passwordLength = int.Parse(Console.ReadLine());
+
+            string password = GeneratePassword(passwordLength);
+            Console.WriteLine("Oluşturulan şifre: " + password);
+
+            Console.Read();
+        }
+
+        static string GeneratePassword(int length)
+        {
+            const string chars =
+                "ABCDEFGHIJKLMNOPQRSTUWXYZabcdefghıjklmnopqrstuwxyz0123456789!@#$%^&*()_*";
+            StringBuilder sb = new StringBuilder();
+            Random random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = random.Next(chars.Length);
+                sb.Append(chars[index]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
